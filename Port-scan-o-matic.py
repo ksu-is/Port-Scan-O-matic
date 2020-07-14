4b2ce4e93c542bc5e887192ad1a69e832fa9f1f7
wwalke32 <65994590+wwalke32@users.noreply.github.com>
2020-07-14 12:40:07 -0400

    added some code using nmap to scan for ports
    no gui yet scanning for open ports in a specific range imported nmap

---
+++ Port-scan-o-matic.py
@@ -0,0 +1,21 @@
+import nmap 
+
+#  range of ports to be scanned 
+begin = 75
+end = 81
+
+#  target ip to be scanned to 
+
+target = '192.168.1.100'
+
+scanner = nmap.PortScanner() 
+
+for i in range(begin,end+1): 
+
+	# scan the target port 
+	res = scanner.scan(target,str(i)) 
+
+	
+	res = res['scan'][target]['tcp'][i]['state'] 
+
+	print(f'port {i} is {res}.') 
