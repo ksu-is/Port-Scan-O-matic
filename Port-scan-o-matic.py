535649c48102aea2dca2013c46890e13ad226193
wwalke32 <65994590+wwalke32@users.noreply.github.com>
2020-07-16 20:32:25 -0400

    Added GUI
    Added a ui and am working on adding in code to get system info and scan for open ports

--- Port-scan-o-matic.py
+++ Port-scan-o-matic.py
@@ -1,23 +1,116 @@
-#adds nmap to this python code 
-import nmap 
+# -*- coding: utf-8 -*-
 
-#  range of ports to be scanned 
-begin = int(input("input port between 1-65,535 to start port scan at: "))
-end = int(input("input port between 1-65,535 to end port scan at: "))
+# Form implementation generated from reading ui file 'Port-Scan-O-Matic.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
 
-#  Input Target IP address 
 
-target = input("Input an IP address to scan for open ports: ")
+from PyQt5 import QtCore, QtGui, QtWidgets
+import nmap
+import subprocess
 
-scanner = nmap.PortScanner() 
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(811, 591)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../../../../Downloads/icon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, -10, 811, 581))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../../../../../Desktop/Port scan bg.jpg"))
+        self.label.setScaledContents(True)
+        self.label.setObjectName("label")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(50, 90, 201, 221))
+        self.textBrowser.setAutoFillBackground(False)
+        self.textBrowser.setObjectName("textBrowser")
+        self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setGeometry(QtCore.QRect(550, 90, 201, 221))
+        self.textBrowser_2.setAutoFillBackground(False)
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(50, 30, 221, 51))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(14)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(550, 30, 221, 51))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(14)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(70, 340, 141, 41))
+        self.pushButton.setCheckable(False)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(570, 380, 151, 51))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(550, 330, 201, 31))
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(600, 520, 281, 41))
+        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-for i in range(begin,end+1): 
+        #Connects click event to systeminfo function
+        self.pushButton.clicked.connect(self.systeminfo)
 
-	# scan the target port 
-	res = scanner.scan(target,str(i)) 
 
-	
-	res = res['scan'][target]['tcp'][i]['state'] 
-#prints the resaults below and end message for the scan
-	print(f'port {i} is {res}.') 
-print( "end of scan, to scan more ports increase port range or do a different scan with a different range")
+def systeminfo(self):
+    #Uses subprocess to run a shell that uses netstat command
+    output = subprocess.run("systeminfo", shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    #stores result and decodes it to utf-8
+    results = output.stdout.decode()
+    #Pushes output into text brower widget
+    self.textBrowser.setText(results)
+
+
+
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Port-Scan-O-Matic"))
+        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\';\"><br /></p></body></html>"))
+        self.textBrowser_2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'MS Shell Dlg 2\';\"><br /></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ffffff;\">System Information</span></p></body></html>"))
+        self.label_3.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; font-weight:600; color:#ffffff;\">Port Scan output</span></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", "Click For System Info"))
+        self.pushButton_2.setText(_translate("MainWindow", "Click to Scan For open ports"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "             Input Target IP Address"))
+        self.label_4.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt; color:#ffffff;\">By: William Walker</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
